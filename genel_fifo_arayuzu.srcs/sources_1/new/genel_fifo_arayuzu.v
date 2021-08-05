f1c1358facb070503652f41f8c75fcea1c9e9640
irem <iremnurcolak34@gmail.com>
2021-08-05 13:53:24 +0300

    rx'ten veri alip veri kuyruguna aktarma ile ilgili duzenlemeler

--- genel_fifo_arayuzu.srcs/sources_1/new/genel_fifo_arayuzu.v
+++ genel_fifo_arayuzu.srcs/sources_1/new/genel_fifo_arayuzu.v
@@ -24,25 +24,28 @@ module genel_fifo_arayuzu(
     input system_clk,system_rst,
     input [31:0] komut,
     input komut_gecerli,
-   // input veri_hazir,
- //   input RX,
+   input veri_hazir,
+   input RX,
     output TX,
-    output reg komut_hazir
-  //  output [31:0] veri,
-   // output veri_gecerli
+    output reg komut_hazir,
+  output [31:0] veri,
+  output veri_gecerli
     );
     reg data_en=0;
     reg change_baudrate=0;
     reg [1:0] baudrate=2'b00;
     wire data_sent;
     wire corrupted;
+    wire [7:0] RX_data;
+    wire RX_data_en;
     UART_transmitter send(system_clk,system_rst,komut[31:24],data_en,change_baudrate,baudrate,TX,data_sent);
-   // UART_receiver    receive(system_clk,system_rst,RX,change_baudrate,baudrate,,corrupted);
+    UART_receiver    receive(system_clk,system_rst,RX,change_baudrate,baudrate,RX_data,RX_data_en,corrupted);
     reg mesgul_tr=0;
     reg mesgul_re=0;
-    wire [3:0] bayt_sent=4'd0;
     
-    reg [31:0] veri_r;
+    assign veri[7:0]=RX_data;
+    assign veri_gecerli=RX_data_en;
+
     always @(posedge system_clk)begin
         if(!system_rst)begin
 
@@ -50,7 +53,7 @@ module genel_fifo_arayuzu(
                 data_en<=1;
                 mesgul_tr<=1;
             end
-            else if(komut_gecerli==1 && komut[2:0]==3'b001&&mesgul_re==0)begin //yaz
+            else if(komut_gecerli==1 && komut[2:0]==3'b001&&mesgul_re==0&&veri_hazir)begin //yaz
           
                 mesgul_re<=1;
             end
@@ -75,6 +78,11 @@ module genel_fifo_arayuzu(
             end
             else if(data_sent==0&&mesgul_tr==0)begin
                 komut_hazir<=0;
+            end
+            
+            if(RX_data_en)begin
+                mesgul_re<=0;
+                komut_hazir<=1;
             end
         end
     end
