e098d68a6d7b78765792c4801a273d98a05851af
iremnurcolak <iremnurcolak34@gmail.com>
2021-08-05 23:17:31 +0300

    reset durumu haric tamam gibi commiti

--- genel_fifo_arayuzu.srcs/sim_1/new/tb_genel_fifo_arayuzu.v
+++ genel_fifo_arayuzu.srcs/sim_1/new/tb_genel_fifo_arayuzu.v
@@ -26,10 +26,13 @@ module tb_genel_fifo_arayuzu(
     reg system_clk,system_rst;
     reg [31:0] komut;
     reg komut_gecerli;
+    reg veri_hazir;
+    reg RX;
     wire TX;
     wire komut_hazir;
-    
-    genel_fifo_arayuzu uut (system_clk,system_rst,komut,komut_gecerli,TX,komut_hazir);
+    wire  [31:0] veri;
+    wire veri_gecerli;
+    genel_fifo_arayuzu uut (system_clk,system_rst,komut,komut_gecerli,veri_hazir,RX,TX,komut_hazir,veri,veri_gecerli);
     always begin
         system_clk = ~system_clk; #5;
     end
@@ -39,7 +42,9 @@ module tb_genel_fifo_arayuzu(
         system_rst=0;
     //    komut=32'b01110001010101010101010101010000; komut_gecerli=1; #250;
       // komut=32'b01010101101010101010101010101000; komut_gecerli=1;   #250;
-        komut=32'b01010101011010101010101010101010; komut_gecerli=1;   #20;
-        komut=32'b01010101101010101010101010101000; komut_gecerli=1;   #250;
+        komut=32'b01010101011010101010101010101001; komut_gecerli=1;   #10;
+        RX=0;#20;RX=1;#20;RX=0;#20;RX=1;#20;RX=0;#20;RX=1;#20;RX=0;#20;RX=1;#20;RX=0;#20; RX=0; #20; RX=1;
+ 
+ //       komut=32'b01010101101010101010101010101000; komut_gecerli=1;   #250;
     end 
 endmodule
